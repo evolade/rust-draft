5eb2703096c14d00815cf351531b9f415d72c4e2
evolade <kutaycereci89@gmail.com>
2021-11-09 12:13:27 +0300

    updated int sizes and declared some vars types

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     println!("EVOLADE IS THE BEST");
     
     // example
-    //println!("{:?}", fib_seq(20)); 
+    //println!("{:?}", fib_seq(32)); 
 }
 
 // a game you prolly already know, if a number can divisible by 3 you say fizz
@@ -44,7 +44,7 @@ fn anagram(_word: &str, _word2: &str) -> bool {
     let mut _word_vec: Vec<char> = vec![];
     let mut _word2_vec: Vec<char> = vec![];
     let mut _is_equal: bool = false;
-    let mut _point: u16 = 0;
+    let mut _point: u8 = 0;
     
     for _i in _word.chars() {
         _word_vec.push(_i);
@@ -88,7 +88,7 @@ fn anagram(_word: &str, _word2: &str) -> bool {
         _is_equal = false;
     }
 
-    if _point == _word_vec_len as u16 * 2 {
+    if _point == _word_vec_len as u8 * 2 {
         return true;
     }
     return false;
@@ -99,7 +99,7 @@ fn anagram(_word: &str, _word2: &str) -> bool {
 fn input(_headline: &str) -> &str{
     print!("{}", _headline);
     std::io::stdout().flush().unwrap(); // idk what this thing does dont remove it
-    let mut _input = String::new();
+    let mut _input: String = String::new();
     std::io::stdin().read_line(&mut _input).expect("err"); // take input
     _input.pop(); // pop the last index ("\n")
     let _str_input: &str = Box::leak(_input.into_boxed_str()); // convert String to &str
@@ -134,7 +134,7 @@ fn log(_base: u16, _num: u16) -> u16 {
 // factorial
 // fact(6); => 720
 fn fact(_num: u16) -> u32{
-    let mut _res:u32 = 1;
+    let mut _res: u32 = 1;
 
     for _i in 0.._num {
         _res = ((_i + 1) * _res as u16) as u32;
@@ -144,18 +144,18 @@ fn fact(_num: u16) -> u32{
 
 // converts binary to decimal
 // bin_to_dec("1011001"); => 89
-fn bin_to_dec(_bin: &str) -> u16{
-    let _r_bin = String::from(_bin.chars().rev().collect::<String>()); // reverse <_bin>
-    
+fn bin_to_dec(_bin: &str) -> u32{
+    let _r_bin: String = String::from(_bin.chars().rev().collect::<String>()); // reverse <_bin>
     let  _r_bin_arr: Vec<char> = _r_bin.chars().collect(); // <_r_bin> to vector
-    let mut _res: u16 = 0;
+    
+    let mut _res: u32 = 0;
     let mut _calc: u16;
     
     for _i in 0.._r_bin.len() {
         _calc = u16::pow(2, _i as u32);
 
         if _r_bin_arr[_i] == '1' {
-            _res += _calc;
+            _res += _calc as u32;
         }
     }
     return _res;
@@ -163,7 +163,7 @@ fn bin_to_dec(_bin: &str) -> u16{
 
 // converts decimal to binary
 // dec_to_bin(89); => "1011001"
-fn dec_to_bin(mut _dec: u16) -> String {
+fn dec_to_bin(mut _dec: u32) -> String {
     let mut _res: String = String::from(""); 
     
     if _dec % 2 == 0 {
@@ -208,11 +208,11 @@ fn find_index(_arr: Vec<&str>, _target: &str) -> u16{
 // prints Fibonacci Sequence
 // it cant return multiple integers so it just prints it
 // fib_seq(15); => "<fibonacci sequence>"
-fn fib_seq(_times: u16) -> Vec<u16> {
-    let mut _fib_seq: Vec<u16> = vec![0, 1]; // I have to declare first and second before the loop
+fn fib_seq(_times: u8) -> Vec<u32> {
+    let mut _fib_seq: Vec<u32> = vec![0, 1]; // I have to declare first and second before the loop
     let mut _first: usize = 1;
     let mut _second: usize = 0;
-    let mut _res: u16;
+    let mut _res: u32;
 
     for _i in 0.._times {
         _res = _fib_seq[_first] + _fib_seq[_second]; // doing the actual fibonacci thing
@@ -225,11 +225,11 @@ fn fib_seq(_times: u16) -> Vec<u16> {
 
 // takes power
 // power(3, 4); => 81
-fn power(_growth_rate: u16, _times: u16) -> u16{
-    let mut _res = _growth_rate;
+fn power(_growth_rate: u16, _times: u16) -> u32{
+    let mut _res: u32 = _growth_rate as u32;
 
     for _i in 0.._times - 1 {
-        _res *= _growth_rate;
+        _res *= _growth_rate as u32;
     }
     return _res;
 }
