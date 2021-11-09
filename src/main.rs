dbea09acc5554eb1258a2f2c2747a0dd5273e7e7
evolade <kutaycereci89@gmail.com>
2021-11-09 10:59:41 +0300

    added return statement to fizzbuzz instead of printing every fizzbuzz

--- src/main.rs
+++ src/main.rs
@@ -4,28 +4,31 @@ fn main() {
     println!("EVOLADE IS THE BEST");
     
     // example
-    //println!("{}", anagram("rust", "ruts")); 
+    //println!("{:?}", fizzbuzz(30)); 
 }
 
 // a game you prolly already know, if a number can divisible by 3 you say fizz
 // if a number can divisible by 5 you say buzz
 // if its both you say fizzbuzz
-// fizzbuzz(30); => "<result>""
-fn fizzbuzz(_reps: u8) {
-    for _i in 1.._reps {
+// fizzbuzz(30); => "<result>"
+fn fizzbuzz(_reps: u8) -> Vec<&'static str> {
+    let mut _res_vec: Vec<&str> = vec![];
+    for _i in 1.._reps + 1 {
         if _i % 3 == 0 && _i % 5 == 0 {
-            println!("FIZZBUZZ");
+            _res_vec.push("FIZZBUZZ");
         }
         else if _i % 3 == 0 {
-            println!("FIZZ");
+            _res_vec.push("FIZZ");
         }
         else if _i % 5 == 0 {
-            println!("BUZZ");
+            _res_vec.push("BUZZ");
         }
         else {
-            println!("{}", _i);
+            _res_vec.push(Box::leak(format!("{}",_i).into_boxed_str())); // convert _i (u8) to String and then to &str
         }
     }
+
+    return _res_vec;
 }
 
 // anagram("rust", "ruts"); => true
